bb8c2813b0b9c27f0fbba3cc9b2699e889d3f832
Igor Shevchenko <cyberskunk@gmail.com>
2017-02-06 12:41:15 +0300

    use connect to serve static assets

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-// get cache file
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT||8080;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, function () {
+    process.stdout.write(`app listening on port ${PORT}\n`);
+});
