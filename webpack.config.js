6ecc20ecf233b3be8b290bd5dc0c2d8f4a1a2223
cyberskunk <cyberskunk@gmail.com>
2018-04-05 12:05:05 +0300

    chore: add ability to generate dependencies open licenses list

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 
 const BundleAlalyzer = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const LicenseChecker = require('@jetbrains/ring-ui-license-checker')
 
 module.exports = env => ({
   mode: env && env.development ? 'development' : 'production',
@@ -10,7 +11,35 @@ module.exports = env => ({
     filename: 'app-bundle.js',
   },
 
-  plugins: env && env.analyze ? [new BundleAlalyzer({analyzerMode: 'static'})] : [],
+  plugins: [
+    env && env.analyze && new BundleAlalyzer({analyzerMode: 'static'}),
+    env &&
+      env.licenses &&
+      new LicenseChecker({
+        filename: 'third-party-licenses.txt',
+        format: params =>
+          params.modules
+            .map(
+              mod => `${mod.name} (${mod.url})
+${mod.license.name} (${mod.license.url})`,
+            )
+            .join('\n\n'),
+        // stackframe has wrong license field in 0.3.1
+        exclude: [/stackframe/],
+        customLicenses: [
+          {
+            name: 'stackframe',
+            version: '0.3.1',
+            url: 'https://www.npmjs.com/package/stackframe',
+            license: {
+              name: 'Unlicense',
+              url: 'http://unlicense.org/',
+            },
+          },
+        ],
+        surviveLicenseErrors: true,
+      }),
+  ].filter(Boolean),
 
   module: {
     rules: [
