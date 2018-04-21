87ffa7c69ac3db70775c073e47b3b4cd4716e10a
cyberskunk <cyberskunk@gmail.com>
2018-04-21 23:56:00 +0300

    add eslint-plugin-compat and fix errors

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import {connect, Provider} from 'react-redux'
 import {render} from 'react-dom'
+import 'core-js/fn/promise'
 
 import SomeApp from './components/App'
 import store from './store'
