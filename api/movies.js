4bd7918dba92c164e6d87971317d0c8bb7c506e2
Igor Shevchenko <cyberskunk@gmail.com>
2019-10-19 01:28:49 +0300

    Try serverless functions

---
+++ api/movies.js
@@ -0,0 +1,25 @@
+import 'whatwg-fetch'
+
+module.exports = async (req, res) => {
+  const movies = await fetch(
+    'https://data.sfgov.org/resource/wwmu-gmzc.json?$select=title,locations',
+  )
+    .then(response => response.json())
+    .then(data =>
+      data
+        // filter out movies without locations
+        .filter(movie => movie.locations)
+        // flatten structure to {`title`=>[locationName,...], ...}
+        .reduce((prev, m) => {
+          if (!prev[m.title]) {
+            prev[m.title] = []
+          }
+
+          prev[m.title].push(m.locations)
+
+          return prev
+        }, {}),
+    )
+
+  return res.json(movies)
+}
--- app/fetch/movies.js
+++ app/fetch/movies.js
@@ -1,20 +1,3 @@
 import 'whatwg-fetch'
 
-export default () =>
-  fetch('https://data.sfgov.org/resource/wwmu-gmzc.json?$select=title,locations')
-    .then(response => response.json())
-    .then(data =>
-      data
-        // filter out movies without locations
-        .filter(movie => movie.locations)
-        // flatten structure to {`title`=>[locationName,...], ...}
-        .reduce((prev, m) => {
-          if (!prev[m.title]) {
-            prev[m.title] = []
-          }
-
-          prev[m.title].push(m.locations)
-
-          return prev
-        }, {}),
-    )
+export default () => fetch('/api/movies').then(response => response.json())
