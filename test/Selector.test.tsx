540f318649dc35d33157c3f481f0d079ba04942a
Igor Shevchenko <igor.shevchenko@relexsolutions.com>
2023-02-10 13:54:56 +0200

    Switch to React 18 rendering API

--- test/Selector.test.tsx
+++ test/Selector.test.tsx
@@ -1,10 +1,11 @@
 /* eslint-env jasmine, jest */
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import Selector from '../app/components/Selector';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Selector handleChange={() => undefined} titles={[]} />, div);
+  const root = createRoot(div);
+  root.render(<Selector handleChange={() => undefined} titles={[]} />);
 });
