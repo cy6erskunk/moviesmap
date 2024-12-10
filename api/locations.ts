f2c60d25790af21cd89a1f9af3c0e9bb00473a0c
Igor Shevchenko <cyberskunk@gmail.com>
2024-12-10 23:28:40 +0200

    Make linter happy

--- api/locations.ts
+++ api/locations.ts
@@ -1,7 +1,9 @@
-import fs from 'fs';
-import path from 'path';
+import fs from 'node:fs';
+import path from 'node:path';
 
 const __dirname = new URL('.', import.meta.url).pathname;
-const locations = JSON.parse(fs.readFileSync(path.join(__dirname, 'locations_data.json'), 'utf8'));
+const locations = JSON.parse(
+  fs.readFileSync(path.join(__dirname, 'locations_data.json'), 'utf8')
+);
 
 export default (req: any, res: any) => res.json(locations);
