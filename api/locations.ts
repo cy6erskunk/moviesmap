29a92184e3fb6445734004f422e9253405cf83cf
Igor Shevchenko <cyberskunk@gmail.com>
2024-12-10 23:08:32 +0200

    Use fs to read json

--- api/locations.ts
+++ api/locations.ts
@@ -1,3 +1,5 @@
-import locations from '../public/locations.json';
+import fs from 'fs';
+
+const locations = JSON.parse(fs.readFileSync('../public/locations.json', 'utf8'));
 
 export default (req: any, res: any) => res.json(locations);
