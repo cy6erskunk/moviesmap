e2a52964cc7ac4e792139c13b6f7b3f4c68984be
Igor Shevchenko <cyberskunk@gmail.com>
2025-05-02 22:52:58 +0300

    Install types for clone

--- app/reducers/movies.ts
+++ app/reducers/movies.ts
@@ -1,4 +1,3 @@
-// @ts-expect-error ts-migrate(7016) FIXME: Could not find a declaration file for module 'clon... Remove this comment to see the full error message
 import clone from 'clone';
 import 'core-js/modules/es.object.assign';
 
